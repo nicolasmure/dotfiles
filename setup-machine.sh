a1a670f7494b9353f13e788e49b7770243e4903b
Nicolas MURE <nicolas.mure@knplabs.com>
2017-08-02 23:50:48 +0200

    composer config

--- setup-machine.sh
+++ setup-machine.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# basic update
 sudo dnf upgrade
+
+# node and npm
 sudo dnf install nodejs
 sudo ln -s /bin/node /usr/local/bin/nodejs
 sudo npm install -g javascript-typescript-langserver
+
+# php and composer
+sudo dnf install php composer
+composer global require felixfbecker/language-server
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
