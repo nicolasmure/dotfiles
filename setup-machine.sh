79b287ea6683d8390a15986268761abdc596bfd6
Nicolas MURE <nicolas.mure@knplabs.com>
2018-04-14 15:04:42 +0200

    sudo dnf install tig

--- setup-machine.sh
+++ setup-machine.sh
@@ -4,6 +4,7 @@
 sudo dnf upgrade
 # install the basics
 sudo dnf install git vim neovim tilix \
+    tig \
     unrar \
     gnome-terminal-nautilus # add a "Open in terminal" shortcut on right click
 
