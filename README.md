475e6714cd336d6d08d71cc98bff9d0a283c6827
Nicolas MURE <nicolas.mure@knplabs.com>
2019-08-23 18:15:04 +0200

    use host var to determine whether install chromecast audio or not

--- README.md
+++ README.md
@@ -17,6 +17,14 @@ It will install ansible and use it to provision the machine.
 
 Then, reboot.
 
+## Options
+
+There is a [`vars/localhost.yml.dist`](/vars/localhost.yml.dist) file which is
+copied to the actual file used by ansible during the first machine setup.
+
+You can copy this file first and adjust the options before provisioning the
+machine.
+
 ## Provisioning a specific task
 
 Each ansible task is tagged so it can be played again by specifying the tag :
--- setup-machine.sh
+++ setup-machine.sh
@@ -5,42 +5,15 @@ set -o pipefail
 set -o nounset
 [[ "${TRACE:-}" != "" ]] && set -o xtrace
 
-# @TODO : make ansible task for chromecast audio installation depending on
-#         an env var.
-
-install_chromecast_audio () {
-    dnf copr enable -y bugzy/mkchromecast
-    dnf install -y \
-        ffmpeg \
-        mkchromecast
-
-    # as pulseaudio starts in userland, do not use systemd but desktop session
-    # instead to start pulseaudio-dlna
-    sudo -u "${SUDO_USER}" mkdir -p "${USER_HOME}/.config/autostart"
-    echo "[Desktop Entry]
-Type=Application
-Name=mkchromecast
-Exec=/usr/bin/mkchromecast -p 10291 --encoder-backend ffmpeg -c wav --sample-rate 44100 --chunk-size 1
-" | sudo -u "${SUDO_USER}" tee -a "${USER_HOME}/.config/autostart/mkchromecast.desktop"
-}
-
-prompt_for_chromecast_audio_installation () {
-    echo "Do you want to install chromecast audio ?"
-    select yn in "Yes" "No"; do
-        case $yn in
-            Yes ) install_chromecast_audio; break;;
-            No ) break;;
-        esac
-    done
-}
-
-################################################################################
-
 install_ansible () {
     sudo dnf install -y \
         ansible \
         pexpect \
         python3
+}
+
+copy_dist_files () {
+    cp -n vars/localhost.yml.dist vars/localhost.yml
 }
 
 provision_localhost () {
@@ -53,6 +26,8 @@ provision_localhost () {
 
 main () {
     install_ansible
+
+    copy_dist_files
 
     provision_localhost
 }
